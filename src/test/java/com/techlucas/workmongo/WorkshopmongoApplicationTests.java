d0e2aed10e1eab6f1be3cc55d0581281483ce063
Lucas Silva <ls609464@gmail.com>
2024-10-15 19:27:38 -0300

    First commit

---
+++ src/test/java/com/techlucas/workmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlucas.workmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
